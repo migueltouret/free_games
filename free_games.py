c2183e88d4e1b95728e2ed85696e45411e86bf24
Touret <miguel_touret@hotmail.com>
2020-09-06 01:06:31 +0100

    Update free_games.py

--- free_games.py
+++ free_games.py
@@ -18,7 +18,7 @@ async def rss_updater(client):
 
 
 async def parse_free():
-    blocked_words = ["Twitch Prime", "IndieGala", "Humble Monthly Subscribers", "GOG Connect", "Humble Choice Subscribers", "Amazon Prime Gaming", "Prime Gaming Giveaways"]
+    blocked_words = ["Twitch Prime", "IndieGala", "Humble Monthly Subscribers", "GOG Connect", "Humble Choice Subscribers", "Prime Gaming"]
 
     NewsFeed = feedparser.parse(
         "https://isthereanydeal.com/rss/specials/eu2/").entries
